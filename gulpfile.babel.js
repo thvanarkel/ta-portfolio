8e3fc5e878f81fe6332c012294b46b0756f0fbdf
thvanarkel <me@thomasvanarkel.nl>
2021-05-18 12:17:04 +0200

    Final gulp implementation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-import { src, dest, watch } from 'gulp';
+import { src, dest, watch, series, parallel } from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
@@ -6,7 +6,21 @@ import gulpif from 'gulp-if';
 import postcss from 'gulp-postcss';
 import sourcemaps from 'gulp-sourcemaps';
 import autoprefixer from 'autoprefixer';
+import imagemin from 'gulp-imagemin';
+import del from 'del';
+import webpack from 'webpack-stream';
+import browserSync from "browser-sync";
+import zip from "gulp-zip";
+import info from "./package.json";
 const PRODUCTION = yargs.argv.prod;
+
+export const watchForChanges = () => {
+  watch('src/scss/**/*.scss', styles);
+  watch('src/images/**/*.{jpg,jpeg,png,svg,gif}', series(images, reload));
+  watch(['src/**/*','!src/{images,js,scss}','!src/{images,js,scss}/**/*'], series(copy, reload));
+  watch('src/js/**/*.js', series(scripts, reload));
+  watch("**/*.php", reload);
+}
 
 export const styles = () => {
   return src('src/scss/theme.scss')
@@ -15,9 +29,79 @@ export const styles = () => {
     .pipe(gulpif(PRODUCTION, postcss([ autoprefixer ])))
     .pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
     .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
-    .pipe(dest('dist/css'));
+    .pipe(dest('dist/css'))
+    .pipe(server.stream());
 }
 
-export const watchForChanges = () => {
-  watch('src/scss/**/*.scss', styles);
+export const images = () => {
+  return src('src/img/**/*.{jpg,jpeg,png,svg,gif}')
+    .pipe(gulpif(PRODUCTION, imagemin()))
+    .pipe(dest('dist/img'));
 }
+
+export const copy = () => {
+  return src(['src/**/*','!src/{images,js,scss}','!src/{images,js,scss}/**/*'])
+    .pipe(dest('dist'));
+}
+
+export const scripts = () => {
+  return src('src/js/bundle.js')
+  .pipe(webpack({
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: []
+            }
+          }
+        }
+      ]
+    },
+    mode: PRODUCTION ? 'production' : 'development',
+    devtool: !PRODUCTION ? 'inline-source-map' : false,
+    output: {
+      filename: 'bundle.js'
+    },
+    externals: {
+      jquery: 'jQuery'
+    },
+  }))
+  .pipe(dest('dist/js'));
+}
+
+export const clean = () => del(['dist']);
+
+const server = browserSync.create();
+export const serve = done => {
+  server.init({
+    proxy: "http://portfolio.test/" // put your local website link here
+  });
+  done();
+};
+export const reload = done => {
+  server.reload();
+  done();
+};
+
+export const compress = () => {
+return src([
+  "**/*",
+  "!node_modules{,/**}",
+  "!bundled{,/**}",
+  "!src{,/**}",
+  "!.babelrc",
+  "!.gitignore",
+  "!gulpfile.babel.js",
+  "!package.json",
+  "!package-lock.json",
+  ])
+  .pipe(zip(`${info.name}.zip`))
+  .pipe(dest('bundled'));
+};
+
+export const dev = series(clean, parallel(styles, images, copy, scripts), serve, watchForChanges)
+export const build = series(clean, parallel(styles, images, copy, scripts), compress)
+export default dev;
