26fae4157a4d041ca7342e318e6101b98ec8b371
thvanarkel <me@thomasvanarkel.nl>
2021-05-18 11:36:30 +0200

    Create autowatch task for css

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,23 @@
-import { src, dest } from 'gulp';
+import { src, dest, watch } from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
 import gulpif from 'gulp-if';
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'autoprefixer';
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = () => {
   return src('src/scss/theme.scss')
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulpif(PRODUCTION, postcss([ autoprefixer ])))
     .pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
     .pipe(dest('dist/css'));
+}
+
+export const watchForChanges = () => {
+  watch('src/scss/**/*.scss', styles);
 }
