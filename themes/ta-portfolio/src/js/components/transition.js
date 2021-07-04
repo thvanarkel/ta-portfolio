c9ed52b355d2004256714f7bfd705f5a38717f40
thvanarkel <me@thomasvanarkel.nl>
2021-07-04 15:33:13 +0200

    Initial card/page layout

--- themes/ta-portfolio/src/js/components/transition.js
+++ themes/ta-portfolio/src/js/components/transition.js
@@ -3,27 +3,43 @@ import anime from 'animejs/lib/anime.es.js';
 
 barba.init({
     debug: true,
+
     transitions: [{
+        //sync: true,
         leave: (data) => {
-            // return new Promise(resolve => {
-            //     anime({
-            //         targets: data.current.container,
-            //         opacity: 0,
-            //         easing: 'linear',
-            //         duration: 500,
-            //         complete: () => {
-            //             resolve();
-            //         }
-            //     });
-            // });
+          lockscreen();
+          return new Promise(resolve => {
+                anime({
+                    targets: data.current.container,
+                    translateY: [100, 1000],
+                    easing: 'spring(1, 80, 10, 0)',
+                    complete: () => {
+                        resolve();
+                    }
+                });
+            });
         },
         enter: (data) => {
-            // anime({
-            //     targets: data.next.container,
-            //     easing: 'linear',
-            //     duration: 500,
-            //     opacity: [0, 1],
-            // });
+            anime({
+                targets: data.next.container,
+                translateY: [1000, 100],
+                easing: 'spring(1, 80, 10, 0)',
+            });
         },
+    },{
+      name: 'to-home',
+      to: { namespace: ['home'] },
+      leave: (data) => {
+        return new Promise(resolve => {
+              anime({
+                  targets: data.current.container,
+                  translateY: [100, 1000],
+                  easing: 'spring(1, 80, 10, 0)',
+                  complete: () => {
+                      resolve();
+                  }
+              });
+          });
+      }
     }]
 });
