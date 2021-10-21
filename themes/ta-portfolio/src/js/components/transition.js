1a08bd4712169bc70c773f100bac0c75ebedebb6
thvanarkel <me@thomasvanarkel.nl>
2021-10-21 17:09:01 +0200

    disable barba

--- themes/ta-portfolio/src/js/components/transition.js
+++ themes/ta-portfolio/src/js/components/transition.js
@@ -1,45 +1,79 @@
 import barba from '@barba/core';
 import anime from 'animejs/lib/anime.es.js';
 
-barba.init({
-    debug: true,
+function lockscreen()
+{
+  var body = document.getElementById("body");
+  body.className += " lock-screen";
+  body.addEventListener('touchmove', handleTouchMove, false);
+}
+function unlock()
+{
+  var body = document.getElementById("body");
+ body.classList.remove("lock-screen");
+ body.removeEventListener('touchmove', handleTouchMove);
+}
 
-    transitions: [{
-        //sync: true,
-        leave: (data) => {
-          lockscreen();
-          return new Promise(resolve => {
-                anime({
-                    targets: data.current.container,
-                    translateY: [100, 1000],
-                    easing: 'spring(1, 80, 10, 0)',
-                    complete: () => {
-                        resolve();
-                    }
-                });
-            });
-        },
-        enter: (data) => {
-            anime({
-                targets: data.next.container,
-                translateY: [1000, 100],
-                easing: 'spring(1, 80, 10, 0)',
-            });
-        },
-    },{
-      name: 'to-home',
-      to: { namespace: ['home'] },
-      leave: (data) => {
-        return new Promise(resolve => {
-              anime({
-                  targets: data.current.container,
-                  translateY: [100, 1000],
-                  easing: 'spring(1, 80, 10, 0)',
-                  complete: () => {
-                      resolve();
-                  }
-              });
-          });
-      }
-    }]
-});
+// barba.init({
+//     debug: true,
+//
+//     transitions: [{
+//         //sync: true,
+//         leave: (data) => {
+//           return new Promise(resolve => {
+//             unlock();
+//             anime({
+//                 targets: data.current.container,
+//                 translateY: [100, 1000],
+//                 easing: 'spring(1, 80, 10, 0)',
+//                 complete: () => {
+//                     resolve();
+//                 }
+//             });
+//           });
+//         },
+//         enter: (data) => {
+//             lockscreen();
+//             anime({
+//                 targets: data.next.container,
+//                 translateY: [1000, 100],
+//                 easing: 'spring(1, 80, 10, 0)',
+//             });
+//         },
+//         beforeOnce: (data) => {
+//           console.log("once!")
+//
+//           data.next.container.style.transform = "translateY(100px)"
+//           // anime({
+//           //     targets: data.next.container,
+//           //     translateY: [1000, 100],
+//           //     easing: 'spring(1, 80, 10, 0)',
+//           // });
+//         },
+//         once: (data) => {
+//
+//         },
+//         afterOnce: (data) => {
+//           //lockscreen();
+//         }
+//     },{
+//       name: 'to-home',
+//       to: { namespace: ['home'] },
+//       leave: (data) => {
+//         return new Promise(resolve => {
+//               anime({
+//                   targets: data.current.container,
+//                   translateY: [100, 1000],
+//                   easing: 'spring(1, 80, 10, 0)',
+//                   complete: () => {
+//                       resolve();
+//                   }
+//               });
+//           });
+//       }
+//     }]
+// });
+//
+// barba.hooks.enter((data) => {
+//   console.log(data.next.namespace);
+// });
